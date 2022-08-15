963a23a422ce61931bc7170d82c13a73604da8cd
Misgan Moges <misganmoges03@gmail.com>
2022-08-15 16:50:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h1>{people.length} birth days</h1>
+        <List people={people} />
+        <button className="btn" onClick={() => setPeople([])}>
+          clear items
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "./css/main.css";
+import "./css/normalize.css";
+
+ReactDOM.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
